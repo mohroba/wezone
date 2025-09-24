774bf40b3ca7ad9ec0d465165c2959990d6c8297
Mohsen Robatjazi <mohroba@gmail.com>
2025-09-25 00:39:18 +0330

    initial setup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Metti\LaravelSms\Facade\SendSMS;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/debug-vite', function () {
+    \Artisan::call('optimize:clear');
+    \Artisan::call('queue:restart');
+    \Artisan::call('storage:link');
+    \Artisan::call('module:migrate');
+    \Artisan::call('migrate');
+    \Artisan::call('db:seed');
+    $path = public_path('build/manifest.json');
+    return response()->json([
+        'public_path' => $path,
+        'public_path' => public_path(),
+        'srorage_path' => storage_path(),
+        'exists' => file_exists($path),
+        'size' => file_exists($path) ? filesize($path) : 0,
+    ]);
 });
+
+Route::get('/test-sms', function () {
+    SendSMS::via('ippanel')
+        ->patternMessage('verify',['code' => '1234'])
+        ->recipients(['09196373450'])
+        ->send();
+});
+
