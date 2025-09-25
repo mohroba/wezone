42e95a6c5158779f34bf1aeb8761e17022119134
Mohsen Robatjazi <mohroba@gmail.com>
2025-09-25 08:12:11 +0330

    initial setup

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', "https://api.wezone.app/"),
+    'url' => env('APP_URL', "https://api.wezone.app"),
 
     /*
     |--------------------------------------------------------------------------
--- config/scribe.php
+++ config/scribe.php
@@ -24,7 +24,7 @@ return [
 
     // The base URL displayed in the docs.
     // If you're using `laravel` type, you can set this to a dynamic string, like '{{ config("app.tenant_url") }}' to get a dynamic base URL.
-    'base_url' => config("app.url","https://api.wezone.app/"),
+    'base_url' => config("app.url","https://api.wezone.app"),
 
     // Routes to include in the docs
     'routes' => [
