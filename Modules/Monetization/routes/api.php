1e24a5b7d8a5d5255fd9f35b802ac858474c4c52
Mohsen Robatjazi <25632948+mohroba@users.noreply.github.com>
2025-10-25 19:54:22 +0330

    Add explicit monetization routes and wallet tests

--- Modules/Monetization/routes/api.php
+++ Modules/Monetization/routes/api.php
@@ -1,12 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Monetization\Http\Controllers\MonetizationController;
+use Modules\Monetization\Http\Controllers\PaymentController;
+use Modules\Monetization\Http\Controllers\PlanController;
+use Modules\Monetization\Http\Controllers\PurchaseController;
+use Modules\Monetization\Http\Controllers\WalletController;
 
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::get('monetizations', [MonetizationController::class, 'index'])->name('monetization.index');
-    Route::post('monetizations', [MonetizationController::class, 'store'])->name('monetization.store');
-    Route::get('monetizations/{monetization}', [MonetizationController::class, 'show'])->name('monetization.show');
-    Route::post('monetizations/{monetization}/update', [MonetizationController::class, 'update'])->name('monetization.update');
-    Route::post('monetizations/{monetization}/delete', [MonetizationController::class, 'destroy'])->name('monetization.destroy');
+Route::middleware(['auth:api'])->prefix('monetization')->group(function (): void {
+    Route::get('plans', [PlanController::class, 'index'])->name('monetization.plans.index');
+
+    Route::post('purchases', [PurchaseController::class, 'store'])->name('monetization.purchases.store');
+    Route::get('purchases/{purchase}', [PurchaseController::class, 'show'])->name('monetization.purchases.show');
+    Route::post('purchases/{purchase}/bump', [PurchaseController::class, 'bump'])->name('monetization.purchases.bump');
+
+    Route::post('purchases/{purchase}/payments/initiate', [PaymentController::class, 'initiate'])->name('monetization.payments.initiate');
+    Route::post('payments/verify', [PaymentController::class, 'verify'])->name('monetization.payments.verify');
+
+    Route::get('wallet', [WalletController::class, 'show'])->name('monetization.wallet.show');
+    Route::post('wallet/top-up', [WalletController::class, 'topUp'])->name('monetization.wallet.top-up');
 });
