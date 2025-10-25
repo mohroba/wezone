c48a09a4a75f91d34cca7c97832f9189947cbe46
Mohsen Robatjazi <25632948+mohroba@users.noreply.github.com>
2025-10-25 15:31:39 +0330

    Add user block management

---
+++ Modules/User/app/Http/Requests/UnblockUserRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Modules\User\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Validator;
+
+class UnblockUserRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return $this->user() !== null;
+    }
+
+    public function rules(): array
+    {
+        return [];
+    }
+
+    public function withValidator(Validator $validator): void
+    {
+        $validator->after(function (Validator $validator): void {
+            $authUser = $this->user();
+            $targetUser = $this->route('user');
+
+            if ($authUser !== null && $targetUser !== null && $authUser->is($targetUser)) {
+                $validator->errors()->add('user', __('You cannot unblock yourself.'));
+            }
+        });
+    }
+
+    public function bodyParameters(): array
+    {
+        return [];
+    }
+}
